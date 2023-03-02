4ec3c5d098b02aea994fb4eddee2aa8e74334d3b
alickwong <95950712+alickwong@users.noreply.github.com>
2023-03-02 14:19:53 +0800

    Create init1.sh

---
+++ init1.sh
@@ -0,0 +1,36 @@
+cd ~/environment
+
+mkdir -p manifests/alb-ingress-controller && cd manifests/alb-ingress-controller
+
+# Final location
+# /home/ec2-user/environment/manifests/alb-ingress-controller
+pwd
+
+eksctl utils associate-iam-oidc-provider \
+    --region ${AWS_REGION} \
+    --cluster eksworkshop-eksctl \
+    --approve
+
+
+curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.4/docs/install/iam_policy.json
+
+aws iam create-policy \
+    --policy-name AWSLoadBalancerControllerIAMPolicy \
+    --policy-document file://iam-policy.json
+
+eksctl create iamserviceaccount \
+--cluster=eksworkshop-eksctl \
+--namespace=kube-system \
+--name=aws-load-balancer-controller \
+--attach-policy-arn=arn:aws:iam::${ACCOUNT_ID}:policy/AWSLoadBalancerControllerIAMPolicy \
+--override-existing-serviceaccounts \
+--region ${AWS_REGION} \
+--approve
+
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+helm repo add eks https://aws.github.io/eks-charts
+
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=eksworkshop-eksctl --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller
+
+kubectl get deployment -n kube-system aws-load-balancer-controller
