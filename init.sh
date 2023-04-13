1b4917667f7824f7e28b7a18a1662b4bfe517c46
alickwong <95950712+alickwong@users.noreply.github.com>
2023-04-14 00:33:31 +1000

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,11 @@ chmod +x ./init1.sh
 chmod +x ./init2.sh
 chmod +x ./init3.sh
 
+export EKS_CLUSTER_NAME=eks-workshop
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account);
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region');
+export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION));
+
 ./init1.sh
 
 sleep 20
